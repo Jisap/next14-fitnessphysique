5a9cc72a8cf61746d28ff7a5011bec32ca0be158
Jorge Matos <jisapjisap@gmail.com>
2024-03-07 20:13:51 +0100

    slider

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -4,22 +4,44 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'
 import 'swiper/css/navigation'
 import CustomButton from './CustomButton';
+import SwiperNavButtons from './SwiperNavButtons';
 
 
 const HeroSlider = () => {
   return (
-    <Swiper>
-      <SwiperSlide className='h-full flex justify-end pt-48'>
-        <div className=''>
-          <h1 className='h1 text-center lg:text-left mb-2'>
-            <span>Where hard</span> work meet success
-          </h1>
-          <p className='text-white italic text-center lg:text-left mb-4'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quas minus quidem nisi 
-          </p>
-          <CustomButton text='Get started' containerStyles='w-[196px] h-[62px]' />
+    <Swiper className='h-full'>
+      <SwiperSlide>
+        <div className='h-full flex justify-end pt-48'>
+          <div className='flex flex-col items-center lg:items-start lg:max-w-[700px]'>
+            <h1 className='h1 text-center lg:text-left mb-2'>
+              <span>Where hard</span> work meet success
+            </h1>
+            <p className='text-white italic text-center lg:text-left mb-4'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quas minus quidem nisi 
+            </p>
+            <CustomButton text='Get started' containerStyles='w-[196px] h-[62px]' />
+          </div>
         </div>
       </SwiperSlide>
+      <SwiperSlide>
+        <div className='h-full flex justify-end pt-48'>
+          <div className='flex flex-col items-center lg:items-start lg:max-w-[700px]'>
+            <h1 className='h1 text-center lg:text-left mb-2'>
+              <span>Where hard</span> work meet success
+            </h1>
+            <p className='text-white italic text-center lg:text-left mb-4'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quas minus quidem nisi
+            </p>
+            <CustomButton text='Get started' containerStyles='w-[196px] h-[62px]' />
+          </div>
+        </div>
+      </SwiperSlide>
+      {/* swiper nav buttons */}
+      <SwiperNavButtons 
+        containerStyles='absolute bottom-2 lg:bottom-0 right-0 h-[130px] w-full lg:w-[700px] z-50 flex justify-center lg:justify-start gap-1' 
+        btnStyles='border border-accent text-white w-[56px] h-[56px] flex justify-center items-center hover:bg-accent transition-all duration-300'
+        iconStyles='text-2xl'  
+      />
     </Swiper>
   )
 }
