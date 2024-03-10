f7d68f95a719b7d7423128494047e2f862db387c
Jorge Matos <jisapjisap@gmail.com>
2024-03-10 20:23:58 +0100

    about

--- components/About.tsx
+++ components/About.tsx
@@ -27,20 +27,38 @@ const featured = [
 const About = () => {
   return (
     <section 
-      className="h-screen pt-8 pb-14 lg:pt-16 lg:pb-28" 
+      className="pt-8 pb-14 lg:pt-16 lg:pb-28" 
       id="about"
     >
       <div className='container mx-auto'>
         <div className='flex flex-col items-center gap-2 mb-8'>
-          <h2 className='h2 text-center'>
+          <motion.h2 
+            variants={fadeIn('up', 0.4)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.2 }}  
+            className='h2 text-center'
+          >
             About us
-          </h2>
-          <p className='max-w-[600px] mx-auto text-center'>
+          </motion.h2>
+          <motion.p 
+            variants={fadeIn('up', 0.6)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.2 }} 
+            className='max-w-[600px] mx-auto text-center'
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, sapiente dolorem ullam qui nulla iste non? Fugiat architecto recusandae doloremque voluptate debitis sint neque laboriosam natus, iure voluptates nostrum in.
-          </p>
+          </motion.p>
         </div>
         {/* featured items */}
-        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mb-16'>
+        <motion.div 
+          variants={fadeIn('up', 0.8)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }} 
+          className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mb-16'
+        >
           {featured.map((feature, index) => {
             return (
               <div 
@@ -55,9 +73,16 @@ const About = () => {
               </div>  
             )
           })}
-        </div>
+        </motion.div>
         {/* achievements */}
-        <Achievements />
+        <motion.div
+          variants={fadeIn('up', 1)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }} 
+        >
+          <Achievements />
+        </motion.div>
       </div>
     </section>
   )
