bd059430ee938aa216d17712ac27e68911faaa5e
Jorge Matos <jisapjisap@gmail.com>
2024-03-13 18:30:15 +0100

    membership

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+import { fadeIn } from "@/lib/variants"
+import { motion } from "framer-motion"
+import MembershipSlider from "./MembershipSlider"
+
 
 
 const Membership = () => {
   return (
-    <section className="h-screen bg-lime-300" id="prices">
-      Membership
+    <section className="py-8 xl:py-0 lg:h-[95vh] bg-membership bg-cover bg-center relative" id="prices">
+      <div className="container mx-auto">
+        <h2 className="h2">Membership</h2>
+        <div>
+          <MembershipSlider />
+        </div>
+      </div>
     </section>
   )
 }
