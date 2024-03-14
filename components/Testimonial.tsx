9a9c2cb5c54f39f53ab0675a8f3fe2354a326427
Jorge Matos <jisapjisap@gmail.com>
2024-03-14 18:47:47 +0100

    pagination styles bullets

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,8 +1,17 @@
+"use client"
+
+import Image from "next/image"
+import { FaQuoteLeft } from "react-icons/fa"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination } from "swiper/modules"
+import 'swiper/css'
+import 'swiper/css/pagination'
+
 
 
 const Testimonial = () => {
   return (
-    <section className="h-[50vh] bg-indigo-300" id="testimonial">
+    <section className="h-[50vh]" id="testimonial">
       Testimonial
     </section>
   )
