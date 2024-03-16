460da56270c8502a1f4c6b89c20802f48019968c
Jorge Matos <jisapjisap@gmail.com>
2024-03-16 12:55:53 +0100

    blog

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -7,6 +7,8 @@ import CustomButton from './CustomButton';
 import SwiperNavButtons from './SwiperNavButtons';
 import { motion } from 'framer-motion'
 import { fadeIn } from '@/lib/variants'
+import Image from 'next/image';
+import Link from 'next/link';
 
 const blogData = [
   {
@@ -16,7 +18,7 @@ const blogData = [
     href: ''
   },
   {
-    img: '/assets/img/blog/post1.jpg',
+    img: '/assets/img/blog/post2.jpg',
     date: 'March 10, 2024',
     title: 'Maintain a perfect structure after workout',
     href: ''
@@ -63,17 +65,56 @@ const Blog = () => {
   return (
     <section className="bg-primary-300 text-white py-24" id="blog">
       <div className='container mx-auto'>
-        <h2 className='h2'>Blog</h2>
+        <h2 className='h2 text-center mb-8'>Blog</h2>
         <div>
-          <Swiper>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 15,
+              },
+              1024: {
+                slidesPerView: 3,
+                
+              },
+              1400: {
+                slidesPerView: 4,
+              },
+            }}
+            className='h-[428px] md:max-w-[660px] lg:max-w-none mb-8'
+          >
             {blogData.map((post, index) => {
               return(
                 <SwiperSlide key={index}>
-                  {post.title}
+                  <div className='flex flex-col justify-start h-full max-w-[320px] mx-auto'>
+                    <Image 
+                      src={post.img}
+                      width={320}
+                      height={266}
+                      alt=''
+                      className='mb-6'
+                    />
+                    <div className='flex flex-col items-start'>
+                      <p className='max-w-[380px] uppercase text-[12px] tracking-[3px] mb-1'>{post.date}</p>
+                      <Link href={post.href} className='hover:text-accent transition-all duration-300'>
+                        <h5 className='h5'>
+                          {post.title}
+                        </h5>
+                      </Link>
+                    </div>
+                  </div>
                 </SwiperSlide>  
               )
             })}
           </Swiper>
+        </div>
+        <div>
+          <CustomButton 
+            containerStyles='block w-[196px] h-[62px] mx-auto'
+            text='View all'
+          />
         </div>
       </div>
     </section>
